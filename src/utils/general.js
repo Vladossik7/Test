5ca1442bbb0723084176ee5d6729f60a459e9b7d
Roman Havryliv <romanhavr@ukr.net>
2025-12-11 22:49:48 +0200

    fixed audio recording; other small fixes; added prettier

--- src/utils/general.js
+++ src/utils/general.js
@@ -1,7 +1,25 @@
+const noteNames = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
-export const getNoteName = (noteNumber) => {
-    const noteNames = [
-      "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B",
-    ];
-    return noteNames[noteNumber % 12];
-  };
+const A4 = 440
+
+export const getNoteName = (noteNumber) => noteNames[noteNumber % 12]
+
+export const frequencyToNote = (frequency) => {
+  if (!frequency) return null
+
+  const halfStepsFromA4 = Math.round(12 * Math.log2(frequency / A4))
+  const midiNumber = 69 + halfStepsFromA4
+
+  if (midiNumber < 21 || midiNumber > 108) return null
+
+  const noteIndex = (midiNumber + 9) % 12
+  const octave = Math.floor((midiNumber - 12) / 12)
+
+  return {
+    frequency: Math.round(frequency * 10) / 10,
+    note: noteNames[noteIndex],
+    octave: octave,
+    midiNumber: midiNumber,
+    fullName: `${noteNames[noteIndex]}${octave}`,
+  }
+}
